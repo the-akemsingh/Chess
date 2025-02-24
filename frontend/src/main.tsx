224771e3e3e1b3d7064bdc5489816b162f588ea7
akem <singhakem03@gmail.com>
2025-02-24 17:11:20 +0530

    landing

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,16 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-import { ClerkProvider } from '@clerk/clerk-react'
+// import { ClerkProvider } from '@clerk/clerk-react'
 
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+// const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+    {/* <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/"> */}
       <App />
-    </ClerkProvider>
+    {/* </ClerkProvider> */}
   </StrictMode>,
 )
